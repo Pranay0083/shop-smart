4bfe8b16be0f44ef8100e46046f39e33a27a10dd
Pranay0083 <0083pranay@gmail.com>
2026-02-23 12:32:40 +0530

    labs-html

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { configDefaults } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,5 +17,6 @@ export default defineConfig({
         globals: true,
         environment: 'jsdom',
         setupFiles: './src/setupTests.js',
+        exclude: [...configDefaults.exclude, 'e2e/*'],
     },
 })
